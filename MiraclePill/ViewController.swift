25e614abc783262c44b6e79848f5e424ce7c54ed
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-02 14:11:58 -0400

    1- Back to default background color 2- Fixed Autolayout of image of added file

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.purple
     }
 
     override func didReceiveMemoryWarning() {
