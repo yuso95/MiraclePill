8db925d7320c16b1a69ad568db9ffdc1fc73f49f
Ousmane Y. Abdou <Yuso@Younoussas-MacBook-Pro.local>
2016-11-03 15:18:58 -0400

    Add a button and a pikerand the piker protocol to handle Piker

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let states = ["Alaska", "Alabama", "California", "Maine", "New York", "Texas"]
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var stateButton: UIButton!
+    
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+     
+        statePicker.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
     }
-
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        stateButton.setTitle(states[row], for: .normal)
+        
+        statePicker.isHidden = true
+    }
 }
-
